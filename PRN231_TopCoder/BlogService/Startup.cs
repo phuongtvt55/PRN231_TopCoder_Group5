441875b1e340ad39faf7ddcaa80fef1c3dd5aab9
Yazt <duongtams2tieuvu@gmail.com>
2023-10-18 18:56:10 +0700

    blog
    blog api

--- PRN231_TopCoder/BlogService/Startup.cs
+++ PRN231_TopCoder/BlogService/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -33,8 +34,11 @@ namespace BlogService
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "BlogService", Version = "v1" });
             });
-            services.AddDbContext<blogServiceContext>();
-        }
+
+            services.AddDbContext<blogServiceContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+        
+    }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
