ae0e48f7acdce14b075fae3f15a07b2a0b355d3b
phuongtvt55 <phuongtvt55@gmail.com>
2023-10-19 08:45:49 +0700

    Megre API

---
+++ PRN231_TopCoder/JobApplicationService/Controllers/JobApplicationController.cs
@@ -0,0 +1,123 @@
+﻿using JobApplicationService.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+
+namespace JobApplicationService.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class JobApplicationController : ControllerBase
+    {
+        private readonly jobApplicationServiceContext _dbContext;
+
+        public JobApplicationController(jobApplicationServiceContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllJobApplications()
+        {
+            try
+            {
+                var jobApplications = _dbContext.JobApplications.ToList();
+                if (jobApplications.Count() == 0)
+                {
+                    return NotFound("There is no available Jobs");
+                }
+                return Ok(jobApplications);
+
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            };
+        }
+
+
+        [HttpGet]
+        public IActionResult GetJobApplication(int id) {
+            try
+            {
+                var jobApplication = _dbContext.JobApplications.Find(id);
+                if (jobApplication == null)
+                {
+                    return NotFound("Cannot find job");
+                }
+                return Ok(jobApplication);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+
+        [HttpPost]
+        public IActionResult PostJobApplication(JobApplication ja)
+        {
+            try
+            {
+                _dbContext.Add(ja);
+                _dbContext.SaveChanges();
+                return Ok("Job application created");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut]
+        public IActionResult PutJobApplication(JobApplication model)
+        {
+            if(model == null)
+            {
+                return BadRequest("Invalid data");
+            }
+
+            try
+            {
+                var job = _dbContext.JobApplications.Find(model.ApplicationId);
+                if (job == null)
+                {
+                    return NotFound("Not found with ID");
+                }
+                job.ApplicationId = model.ApplicationId;
+                job.UserId = model.UserId;
+                job.JobId = model.JobId;
+                job.ApplyDate = model.ApplyDate;
+                job.IsDelete = model.IsDelete;
+                job.Status = model.Status;
+                _dbContext.SaveChanges();
+                return Ok("Updated");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            try
+            {
+                var ja = _dbContext.JobApplications.Find(id);
+                if (ja == null)
+                {
+                    return NotFound("Cannot find job with ID");
+                }
+                _dbContext.JobApplications.Remove(ja);
+                _dbContext.SaveChanges();
+                return Ok("Job Application Delete");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
