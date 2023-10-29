51cf853d7604424fe8767c57350eaa78e4cfd143
Yazt <93716592+KitsuYazt@users.noreply.github.com>
2023-10-29 19:08:26 +0700

    blog+comment controller

--- PRN231_TopCoder/Client/Startup.cs
+++ PRN231_TopCoder/Client/Startup.cs
@@ -1,6 +1,9 @@
+using BlogService.Models;
+using JobService.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,6 +27,8 @@ namespace Client
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddHttpClient();
+            services.AddTransient<blogServiceContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
